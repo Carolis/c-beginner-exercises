62c5ab35d1e84d342828c3220152d12bf4fb8f82
Carolina Ale <redrumjedi@gmail.com>
2021-02-28 21:47:55 -0300

    day one - exe7

---
+++ 1/Exe7.c
@@ -0,0 +1,13 @@
+//Atividade 1 - Exe1
+//Carolina Ale 002201701385
+#include <stdio.h>
+
+int main()
+{
+    int a, b, resultado;
+    printf("Digite os dois valores a serem multiplicados\n");
+    scanf("%d", &a);
+    scanf("%d", &b);
+    resultado = a * b;
+    printf("Resultado: %d \n", resultado);
+}
